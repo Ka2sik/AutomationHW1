64d4686107a215113c5cc72b0eac01773f640043
Ekaterina Prudnikova <k_pr_89@mail.ru>
2024-09-09 22:23:28 +0300

    Add tests junit jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,28 +1,28 @@
 package ru.netology.service;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void testRemainShouldReturn200WithAmount800() {
-        Assert.assertEquals(service.remain(800), 200);
+        Assert.assertEquals(200, service.remain(800));
     }
 
     @Test
     public void testRemainShouldReturn0WithAmount1000() {
-        Assert.assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(0, service.remain(1000));
     }
 
     @Test
     public void testRemainShouldReturn999WithAmount1001() {
-        Assert.assertEquals(service.remain(1001), 999);
+        Assert.assertEquals(999, service.remain(1001));
     }
 
     @Test
     public void testRemainShouldReturn1WithAmount999() {
-        Assert.assertEquals(service.remain(999), 1);
+        Assert.assertEquals(1, service.remain(999));
     }
 }
